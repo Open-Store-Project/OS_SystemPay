1b4d1a1cf646e97a1aef5245bb43f73f71ff05de
leedavi <dcl@nevoweb.com>
2018-05-06 06:43:19 +0200

    init v1.0.0

---
+++ Payment.ascx
@@ -0,0 +1,4 @@
+<%@ Control language="C#" Inherits="OS_SystemPay.OS_SystemPayPayment" AutoEventWireup="true"  Codebehind="OS_SystemPayPayment.ascx.cs" %>
+<asp:PlaceHolder ID="phData" runat="server"></asp:PlaceHolder>
+
+
---
+++ Themes/config/js/OS_SystemPay.js
@@ -0,0 +1,40 @@
+﻿
+
+$(document).ready(function () {
+
+    $('#OS_SystemPay_cmdSave').unbind("click");
+    $('#OS_SystemPay_cmdSave').click(function () {
+        $('.processing').show();
+        $('.actionbuttonwrapper').hide();
+        // lower case cmd must match ajax provider ref.
+        nbxget('os_systempay_savesettings', '.OS_SystemPaydata', '.OS_SystemPayreturnmsg');
+    });
+
+    $('.selectlang').unbind("click");
+    $(".selectlang").click(function () {
+        $('.editlanguage').hide();
+        $('.actionbuttonwrapper').hide();
+        $('.processing').show();
+        $("#nextlang").val($(this).attr("editlang"));
+        // lower case cmd must match ajax provider ref.
+        nbxget('os_systempay_selectlang', '.OS_SystemPaydata', '.OS_SystemPaydata');
+    });
+
+
+    $(document).on("nbxgetcompleted", OS_SystemPay_nbxgetCompleted); // assign a completed event for the ajax calls
+
+    // function to do actions after an ajax call has been made.
+    function OS_SystemPay_nbxgetCompleted(e) {
+
+        $('.processing').hide();
+        $('.actionbuttonwrapper').show();
+        $('.editlanguage').show();
+
+        if (e.cmd == 'os_systempay_selectlang') {
+                        
+        }
+
+    };
+
+});
+
